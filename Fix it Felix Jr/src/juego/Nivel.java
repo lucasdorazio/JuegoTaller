9db3c17f72671e1ff6885e1254cbd8f4c37d9ce3
Lucas <Lucas@LAPTOP-QP73SVLA>
2019-10-05 00:56:10 -0300

    Mod. Vent cerrada

--- Fix it Felix Jr/src/juego/Nivel.java
+++ Fix it Felix Jr/src/juego/Nivel.java
@@ -2,51 +2,63 @@ package juego;
 
 public class Nivel {
 	
+	private static final double PROB_VENT_CERRADA=0.066;
+	
 	private int nroNivel;
 	
 	private int tiempoMax;
 	
 	public Edificio generarEdificio() {
-		Seccion seccion1= generarSeccion(Dificultad.getProbPanelesRotos(nroNivel),
+		Seccion seccion1 = generarSeccion(Dificultad.getProbPanelesRotos(nroNivel),
 				Dificultad.getProbObstaculos(nroNivel), Dificultad.getProbVemtAbierta(nroNivel));
-		//hacer metodo generarPlantaBaja
-		
-		
+		// hacer metodo generarPlantaBaja
+
 		Edificio ed = new Edificio();
 		return ed;
 	}
 
 	private Seccion generarSeccion(double probPanelRoto, double probObstaculo,
 			double probVentAbierta) {
-		Ventana v;
+		Ventana[][] matriz = new Ventana[3][5];
 		EstadoPanel inferior, superior;
 		boolean moldura= false;
 		boolean macetero= false;
+		boolean esCerrada=false;
 		inferior = EstadoPanel.SANO;
 		superior = EstadoPanel.SANO;
 		for (int m = 0; m < 3; m++) {
 			for (int n = 0; n < 5; n++) {
-				if (Math.random() <= probPanelRoto/ 4) {
-					inferior = EstadoPanel.ROTO;
-				} else if (Math.random() <= probPanelRoto / 4) {
-					inferior = EstadoPanel.MEDIO_ROTO;
+				esCerrada=Math.random()<=PROB_VENT_CERRADA;
+				if (esCerrada=true) {
+					if (Math.random() <= probPanelRoto/ 4) {
+						inferior = EstadoPanel.ROTO;
+					} else if (Math.random() <= probPanelRoto / 4) {
+						inferior = EstadoPanel.MEDIO_ROTO;
+					}
+					if (Math.random() <= probPanelRoto / 4) {
+						superior = EstadoPanel.ROTO;
+					} else if (Math.random() <= probPanelRoto/ 4) {
+						superior = EstadoPanel.MEDIO_ROTO;
+					}
+					if (superior!=EstadoPanel.SANO || inferior!=EstadoPanel.SANO) {
+						
+					}
 				}
-				if (Math.random() <= probPanelRoto / 4) {
-					superior = EstadoPanel.ROTO;
-				} else if (Math.random() <= probPanelRoto/ 4) {
-					superior = EstadoPanel.MEDIO_ROTO;
-				}
+				
 				if (Math.random()<= probObstaculo /2) {
 					macetero=true;
 				}
 				if (Math.random()<= probObstaculo /2) {
 					moldura=true;
 				}
-				if (Math.random()<=probVentAbierta) {
-					v= new ConHojas(m, n, macetero, moldura, inferior, superior, true);
+	
+				if (esCerrada) {
+					matriz[m][n]= new ConHojas(m, n, macetero, moldura, inferior, superior, false);
+				} else if (Math.random()<=probVentAbierta) {
+					matriz[m][n]= new ConHojas(m, n, macetero, moldura, inferior, superior, true);
+				} else {
+					matriz[m][n]= new Comun(m, n, true, false, inferior, superior);
 				}
-				v = new Comun(m, n, true, false, inferior, superior);
-
 			}
 		}
 	}
