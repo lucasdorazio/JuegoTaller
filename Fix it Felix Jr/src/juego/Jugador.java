ec8ce2e3d4842ef4181d01776fb456a78b1f8880
Lucas <Lucas@LAPTOP-QP73SVLA.utopia.net>
2019-10-09 22:06:10 -0300

    jugador

---
+++ Fix it Felix Jr/src/juego/Jugador.java
@@ -0,0 +1,27 @@
+package juego;
+
+public class Jugador {
+	
+	private String nick;
+	
+	private int puntaje;
+
+	public String getNick() {
+		return nick;
+	}
+
+	public int getPuntaje() {
+		return puntaje;
+	}
+
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+
+	public void setPuntaje(int puntaje) {
+		this.puntaje = puntaje;
+	}
+	
+	
+
+}
